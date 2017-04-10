c3a3a4b52ccad0119ef776dfcc84d8adaa62a614
vxhly <904334946@qq.com>
2017-04-10 19:48:09 +0800

    base-flex@2.1.1

---
+++ README.md
@@ -0,0 +1,26 @@
+# base-flex
+
+> 重置浏览器默认的一些样式，包含 flexbox 布局，使用 scss 语法编写
+
+### 参考
+
+> <https://github.com/lzxb/flex.css>
+>
+> <https://github.com:vxhly/scss-flex>
+>
+> <https://github.com/necolas/normalize.css>
+>
+> <https://github.com/amazeui/amazeui>
+
+
+### download
+
+    git clone https://github.com/vxhly/scss-flex.git
+
+### npm
+
+    npm install scss-flex --save
+
+### bower
+
+    bower install scss-flex --save
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+/**
+ * 引入 gulp
+ */
+var gulp = require('gulp'); // 基础库
+
+/**
+ * gulp 模块化管理工具
+ */
+var gulpLoadPlugins = require('gulp-load-plugins'); // 模块化管理
+var $ = gulpLoadPlugins(); // 定义变量
+
+var scssSrc = './scss/base.scss';
+
+gulp.task('default', function () {
+  return gulp.src(scssSrc)
+    .pipe($.sass({
+        outputStyle: 'expanded'
+      })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: [
+        'ie >= 8',
+        'ie_mob >= 10',
+        'ff >= 26',
+        'chrome >= 30',
+        'safari >= 6',
+        'opera >= 23',
+        'ios >= 5',
+        'android >= 2.3',
+        'bb >= 10'
+      ]
+    }))
+    .pipe($.csscomb())
+    .pipe($.cssbeautify({
+      indent: '  ',
+      openbrace: 'end-of-line',
+      autosemicolon: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
+    .pipe($.cleanCss({
+      debug: true,
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('./dist'))
+});
